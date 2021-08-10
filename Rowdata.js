6963011569f2de345267fc08e36b55a77b13a56b
Aniket Singh <61254165+aniketsingh16@users.noreply.github.com>
2021-08-10 11:50:29 +0530

    Create Rowdata.js

---
+++ Rowdata.js
@@ -0,0 +1,33 @@
+export const RowsData = [
+    {
+        "id": 1,
+        "employee_name":"Aniket Singh",
+        "employee_salary":"$65000",
+        "employee_age":"43"
+    },
+    
+    {
+        "id": 2,
+        "employee_name":"Alex Mathew",
+        "employee_salary":"$29000",
+        "employee_age":"34"
+    },
+    {
+        "id": 3,
+        "employee_name":"Dakota Jhonson",
+        "employee_salary":"$288000",
+        "employee_age":"31"
+    },
+    {
+        "id": 4,
+        "employee_name":"Philip Dale",
+        "employee_salary":"$98000",
+        "employee_age":"38"
+    },
+    {
+        "id": 5,
+        "employee_name":"Justin Williamson",
+        "employee_salary":"$76000",
+        "employee_age":"36"
+    }
+]
