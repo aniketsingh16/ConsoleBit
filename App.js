1d47f86227a9e0ee5b4f5e0ebb6b60977efebc4b
Aniket Singh <61254165+aniketsingh16@users.noreply.github.com>
2021-08-10 11:52:16 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Homeview from './Cbit/Homeview';
+import Navigation from './Cbit/Navigation'
+
+
+function App() {
+  return (
+    <div className="App"> 
+      <Homeview />
+      </div>
+  );
+}
+
+export default App;
